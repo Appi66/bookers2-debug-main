e469437d9e443129028883486299313e08b42d89
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-01-31 06:35:44 +0000

    [Add]検索機能しかし結果一覧未完成

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,30 @@
+class SearchesController < ApplicationController
+  before_action :authenticate_user!
+
+  def search
+    @range = params[:range]
+
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+      redirect_to searchindex_path
+    else
+      @books = Book.looks(params[:search], params[:word])
+      redirect_to searchindex_path
+    end
+  end
+
+  def index
+    @range = params[:range]
+
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+
+    else
+      @books = Book.looks(params[:search], params[:word])
+
+    end
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,9 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
-  get "search" => "searches#sear"
+  get "search" => "searches#search"
+  get "search/index" => "searches#index", as: 'searchindex'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
