5568862f8af0e4fe775a25ab2af07545c9cf23b8
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-02-06 06:39:37 +0000

    [Add]スターのアソシエーションまで

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_104550) do
+ActiveRecord::Schema.define(version: 2022_02_06_055005) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,14 @@ ActiveRecord::Schema.define(version: 2022_01_30_104550) do
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "star"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
