905390840f7f36a1eac6e235365bf5bf6d1b055f
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-02-07 13:33:48 +0000

    [update]星評価途中

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require jquery.raty.js
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -21,7 +23,7 @@ require("channels")
 require("jquery")
 
 window.$ = window.jQuery = require('jquery');
-require('packs/raty')
+require('packs/jquery.raty')
 
 Rails.start()
 Turbolinks.start()
